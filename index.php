98acc300ec27c4b88f6e1db54a69f3082228a035
diegomorane <diegomorane>
2022-05-26 14:51:15 -0500

    configuracion controladores y acciones por defecto y redireccionamiento a vista de error

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php
 //Cargamos el autoload, teniendo acceso a todos los controladores, a todas las clases
 require_once 'autoload.php';
+
+require_once 'config/parameters.php';
 //requerimos maqueta html
 require_once 'views/layout/header.php';
 require_once 'views/layout/sidebar.php';
 
+function show_error()
+{
+    $error = new errorController();
+    $error->index();
+}
+
 //verificamos si llega el controlador por la url
-if (isset($_GET['controllers'])) {
-    $autocargar = $_GET['controllers'] . 'Controller';
+if (isset($_GET['controller'])) {
+    $autocargar = $_GET['controller'] . 'Controller';
+} elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+    $autocargar = controller_default;
 } else {
-    echo "La pagina que buscas no existe";
+    show_error();
     exit();
 }
 
@@ -21,11 +31,14 @@ if (class_exists($autocargar)) {
     if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
         $action = $_GET['action'];
         $controlador->$action();
+    } elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+        $action_default = action_default;
+        $controlador->$action_default();
     } else {
-        echo "La pagina que buscas no existe";
+        show_error();
     }
 } else {
-    echo "La pagina que buscas no existe";
+    show_error();
 }
 
 require_once 'views/layout/footer.php';
