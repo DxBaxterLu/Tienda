04624d585f8b3ccec849ab18aa226ab92c177438
diegomorane <diegomorane>
2022-05-27 17:01:42 -0500

    Muestra de productos aleatorios

--- views/producto/destacados.php
+++ views/producto/destacados.php
@@ -1,19 +1,16 @@
-<h1>Productos Destacados</h1>
-<div class="product">
-    <img src="assets/img/camiseta.png">
-    <h2>Camiseta Azul Ancha</h2>
-    <p>$30 Dolares</p>
-    <a href="#" class="button">Comprar</a>
-</div>
-<div class="product">
-    <img src="assets/img/camiseta.png">
-    <h2>Camiseta Azul Ancha</h2>
-    <p>$30 Dolares</p>
-    <a href="#" class="button">Comprar</a>
-</div>
-<div class="product">
-    <img src="assets/img/camiseta.png">
-    <h2>Camiseta Azul Ancha</h2>
-    <p>$30 Dolares</p>
-    <a href="#" class="button">Comprar</a>
-</div>
+<h1>Algunos de nuestros productos</h1>
+
+<?php while ($product = $productos->fetch_object()) : ?>
+    <div class="product">
+        <a href="<?= base_url ?>producto/ver&id=<?= $product->id ?>">
+            <?php if ($product->imagen != null) : ?>
+                <img src="<?= base_url ?>uploads/images/<?= $product->imagen ?>" />
+            <?php else : ?>
+                <img src="<?= base_url ?>assets/img/camiseta.png" />
+            <?php endif; ?>
+            <h2><?= $product->nombre ?></h2>
+        </a>
+        <p><?= $product->precio ?></p>
+        <a href="#" class="button">Comprar</a>
+    </div>
+<?php endwhile; ?>
