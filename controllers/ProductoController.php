fba24af7d2826875c0d32299af3b6b043248243c
diegomorane <diegomorane>
2022-05-27 15:38:57 -0500

    Creacion de controlador de prodcutos y sus metodos CRUD

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -43,6 +43,21 @@ class ProductoController
                 $producto->setStock($stock);
                 $producto->setCategoria_id($categoria);
 
+                //Guardar
+                $file = $_FILES['imagen'];
+                $filename = $file['name'];
+                $mimetype = $file['type'];
+
+                if ($mimetype == "image/jpg" || $mimetype == 'image/jpeg' || $mimetype == 'image/png' || $mimetype == 'image/gif') {
+
+                    if (!is_dir('uploads/images')) {
+                        mkdir('uploads/images', 0777, true);
+                    }
+
+                    $producto->setImagen($filename);
+                    move_uploaded_file($file['tmp_name'], 'uploads/images/' . $filename);
+                }
+
                 $save = $producto->save();
 
                 if ($save) {
@@ -56,6 +71,33 @@ class ProductoController
         } else {
             $_SESSION['producto'] = 'Failed';
         }
+        header('Location:' . base_url . 'producto/gestion');
+    }
+
+    public function editar()
+    {
+        var_dump($_GET);
+    }
+
+    public function eliminar()
+    {
+        Utils::isAdmin();
+
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+            $producto = new Producto();
+            $producto->setId($id);
+
+            $delete = $producto->delete();
+            if ($delete) {
+                $_SESSION['delete'] = 'complete';
+            } else {
+                $_SESSION['delete'] = 'failed';
+            }
+        } else {
+            $_SESSION['delete'] = 'failed';
+        }
+
         header('Location:' . base_url . 'producto/gestion');
     }
 }
