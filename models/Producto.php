33a68287c0b89240ebd57456a521e001036b4930
diegomorane <diegomorane>
2022-05-27 12:54:43 -0500

    Se creo el modelo de productos consus respectivos metodos get y ser, ademas del metodo save y getAll

---
+++ models/Producto.php
@@ -0,0 +1,123 @@
+<?php
+
+class Producto
+{
+    private $id, $categoria_id, $nombre, $descripcion, $precio, $stock, $oferta, $fecha, $imagen, $db;
+
+    public function __construct()
+    {
+        $this->db = Database::connect();
+    }
+
+    //Metodos Get
+    function getId()
+    {
+        return $this->id;
+    }
+
+    function getCategoria_id()
+    {
+        return $this->categoria_id;
+    }
+
+    function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    function getStock()
+    {
+        return $this->stock;
+    }
+
+    function getOferta()
+    {
+        return $this->oferta;
+    }
+
+    function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    function getImagen()
+    {
+        return $this->imagen;
+    }
+
+    //Metodos Set
+    function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    function setCategoria_id($categoria_id)
+    {
+        $this->categoria_id = $categoria_id;
+    }
+
+    function setNombre($nombre)
+    {
+        $this->nombre = $this->db->real_escape_string($nombre);
+    }
+
+    function setDescripcion($descripcion)
+    {
+        $this->descripcion = $this->db->real_escape_string($descripcion);
+    }
+
+    function setPrecio($precio)
+    {
+        $this->precio = $this->db->real_escape_string($precio);
+    }
+
+    function setStock($stock)
+    {
+        $this->stock = $this->db->real_escape_string($stock);
+    }
+
+    function setOferta($oferta)
+    {
+        $this->oferta = $this->db->real_escape_string($oferta);
+    }
+
+    function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+    }
+
+    function setImagen($imagen)
+    {
+        $this->imagen = $imagen;
+    }
+
+    //Otros Metodos
+    public function getAll()
+    {
+        $productos = $this->db->query('SELECT * FROM productos ORDER BY id desc;');
+        return $productos;
+    }
+
+    public function save()
+    {
+        $sql = "INSERT INTO productos VALUES(NULL, '{$this->getCategoria_id()}', '{$this->getNombre()}', '{$this->getDescripcion()}', {$this->getPrecio()}, {$this->getStock()}, NULL, CURDATE(), NULL)";
+
+        $save = $this->db->query($sql);
+
+        $result = false;
+        if ($save) {
+            $result = true;
+        }
+        return $result;
+    }
+}
