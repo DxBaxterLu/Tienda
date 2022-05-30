5a31c125993731fa08e98578c3420aeb12cdf4cd
Baxter <dave.f7.mart@gmail.com>
2022-05-29 21:58:07 -0500

    se añadio el link al carrito de compras y modificacion de links

--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -1,5 +1,15 @@
 <!-- barra lateral -->
 <aside id="lateral">
+    <div id="carrito" class="block_aside">
+		<h3>Mi carrito</h3>
+		<ul>
+			<?php $stats = Utils::statsCarrito(); ?>
+			<li><a href="<?=base_url?>carrito/index">Productos (<?=$stats['count']?>)</a></li>
+			<li><a href="<?=base_url?>carrito/index">Total: <?=$stats['total']?> $</a></li>
+			<li><a href="<?=base_url?>carrito/index">Ver el carrito</a></li>
+		</ul>
+	</div>
+
     <div id="login" class="block_aside">
         <?php if (!isset($_SESSION['identity'])) : ?>
             <h3>Ingresa a la Web</h3>
@@ -17,11 +27,11 @@
             <?php if (isset($_SESSION['admin'])) : ?>
                 <li><a href="<?= base_url; ?>categoria/index">Gestionar Categorias</a></li>
                 <li><a href="<?= base_url; ?>producto/gestion">Gestionar Productos</a></li>
-                <li><a href="#">Gestionar Pedidos</a></li>
+                <li><a href="<?=base_url?>pedido/gestion">Gestionar pedidos</a></li>
             <?php endif; ?>
 
             <?php if (isset($_SESSION['identity'])) : ?>
-                <li><a href="#">Mis Pedidos</a></li>
+                <li><a href="<?=base_url?>pedido/mis_pedidos">Mis Pedidos</a></li>
                 <li><a href="<?= base_url; ?>usuario/logout">Cerrar Session</a></li>
             <?php else : ?>
                 <li><a href="<?= base_url; ?>usuario/registro">Registrarse</a></li>
